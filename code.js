1b3bfec7d5c468610d6ad4bdf4768286b2d9d831
Dev Pratap Singh <devthakur31931@gmail.com>
2024-01-16 15:56:06 +0530

    Building project in js

---
+++ code.js
@@ -0,0 +1,31 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    AUD: "AU",
+ 
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+ 
+    INR: "IN",
+   
+    JMD: "JM",
+   
+    JPY: "JP",
+ 
+   
+    NPR: "NP",
+    NZD: "NZ",
+ 
+    PAB: "PA",
+   
+   
+    UGX: "UG",
+    USD: "US",
+   
+   
+  };
+
+
+  
